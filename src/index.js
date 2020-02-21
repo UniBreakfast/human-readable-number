e8be783104a0ebb55d1f744b2dad78df2aa79c14
unibreakfast <unibreakfast1@gmail.com>
2020-02-21 21:48:21 +0200

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(num) {
+  if (!num) return 'zero'
+  const words = []
+  if (num>99) words.push(nums[num/100|0], 'hundred')
+  if (num%100>0 && num%100/10>=2) words.push(tens[num%100/10|0])
+  if (num%100 && num%100<20) words.push(nums[num%100])
+  else if (num%10) words.push(nums[num%10])
+  return words.join(' ')
 }
+
+const nums = ('zero,one,two,three,four,five,six,seven,eight,nine,ten,' +
+              'eleven,twelve,thirteen,fourteen,fifteen,sixteen,' +
+              'seventeen,eighteen,nineteen').split(',')
+const tens = 'zero,ten,twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety'
+              .split(',')
